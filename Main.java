7ac0a558748a32261aaccc14e0831fafb38f37ff
Abdallah7x <76527450+Abdallah7x@users.noreply.github.com>
2023-11-07 10:39:59 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
-  static void myMethod() {
-    System.out.println("Hello World!");
-  }
+ public static void main(String[] args)
+    {
+        System.out.println("worked");
+    }
 }
